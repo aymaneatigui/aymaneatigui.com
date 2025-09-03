2749632b66edcf469debe31ebc5e6cfe3c5edd8b
aymaneatigui <aymane@bill-app.fr>
2025-09-03 20:39:01 +0100

    chore: initialize project with Next.js, TypeScript, and Tailwind CSS

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-4xl font-bold">aymane atigui</h1>
+    </div>
+  );
+}
---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  distDir: 'build',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
